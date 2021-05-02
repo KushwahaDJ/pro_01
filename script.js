009f4c99333739f1131b1c7ec6517535aa9b5e3b
Dhananjay Kushwaha <37621855+KushwahaDJ@users.noreply.github.com>
2021-05-02 20:46:58 +0530

    Add files via upload
    done as per requirment

---
+++ script.js
@@ -0,0 +1,67 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+ function findEmail(evt, findMail) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(findMail).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+
+
+// verify email 
+
+var slider1 = document.getElementById("verifyMe");
+var output1 = document.getElementById("demo1");
+output1.innerHTML = slider1.value;
+
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+
+    function verifyEmail(evt, verifyMmail) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(verifyMmail).style.display = "block";
+      evt.currentTarget.className += " active";
+  }
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click(); 
+
+
+
+// accordian script
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
